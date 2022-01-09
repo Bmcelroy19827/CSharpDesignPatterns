2f278a83d545ff9189fc16f7becd75b0fcbc3de8
Bmcelroy19827 <angelasman2004@yahoo.com>
2022-01-09 09:27:35 -0500

    Created ObjectPoolCL to replace ObjectPool so I can use new PatternPicker Console application

---
+++ DesignPatterns/ObjectPoolCL/CoreBits/ObjectPool.cs
@@ -0,0 +1,98 @@
+﻿using HelpingHand;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPoolCL.CoreBits
+{
+    // https://www.infoworld.com/article/3221392/how-to-use-the-object-pool-design-pattern-in-c.html
+    // Used to help improve performance by queuing up to a (configurable) maximum number of expensive classes (one class type)
+    public class ObjectPool<T> where T : ISharedObject, new()
+    {
+        //ConcurrentBack:- lock-free, thread-safe, unordered collection of elements, inserting and removing elements from this is very fast
+        private readonly ConcurrentBag<T> items = new ConcurrentBag<T>();
+
+        //Counter/max used to control how objects are created 
+        // If user asks for a new object and the Max is already hit you can handle as you see fit
+        // some common options: 1. Return null or throw exception, 2. Block the call until an object is available, 3. Increase the pool size to accomodate more objects
+        private int counter = 0;
+        private int max = 10;
+        public ObjectPool(int numberToCache)
+        {
+            if (numberToCache > 0)
+            {
+                for (int i = 0; i < Math.Min(numberToCache, max); i++)
+                {
+                    items.Add(new T());
+                    counter++;
+                }
+            }
+        }
+        public ObjectPool() : this(0)
+        {
+
+        }
+
+        //used to release objects that are no longer needed back to the object pool
+        public void Release(T item)
+        {
+            if (items.Count < max)
+            {
+                item.Reset();
+                items.Add(item);
+                Console.WriteLine($"Item added to bag\nTotal items in bag: {items.Count}");
+                // removed below as it does not keep track of the total objects in the object pool anymore
+                //counter++;
+            }
+            else
+            {
+                Console.WriteLine($"Bag full\nTotal items in bag: {items.Count}");
+            }
+            Console.WriteLine($"Counter: {counter}");
+        }
+
+        // returns an instance from the object pool if one is available, otherwise a new object is created and returned
+        public T? Get()
+        {
+            T? item;
+            if (items.TryTake(out item))
+            {
+                // removed this so we can keep track of the total objects made
+                //counter--;
+                Console.WriteLine($"Item taken from bag\nTotal items in bag: {items.Count}");
+                Console.WriteLine($"Counter: {counter}");
+                return item;
+            }
+            else
+            {
+                if (counter < max)
+                {
+                    T obj = new T();
+                    //Note: Removed the next line as it allows for duplicate references on an object. Left counter increment so we could use it to limit the number of objects
+                    //items.Add(obj);
+                    counter++;
+                    Console.WriteLine($"Item added to bag\nTotal items in bag: {items.Count}");
+                    Console.WriteLine($"Counter: {counter}");
+                    return obj;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public void showLastMethodOnAllStoredObjects()
+        {
+            HConsole.StarRow();
+            foreach (var item in items)
+            {
+                item?.LastMethodCalled();
+            }
+            HConsole.StarRow();
+        }
+    }
+}
---
+++ DesignPatterns/ObjectPoolCL/OPMain.cs
@@ -0,0 +1,110 @@
+﻿using ObjectPoolCL.CoreBits;
+using HelpingHand;
+
+namespace ObjectPoolCL
+{
+    public class OPMain
+    {
+        private bool callMethodA;
+        private ObjectPool<SharedObject> objPool;
+        private Stack<SharedObject> sOStack;
+
+        public OPMain()
+        {
+            callMethodA = true;
+            objPool = new ObjectPool<SharedObject>(5);
+            sOStack = new Stack<SharedObject>();
+        }
+
+        public void Start()
+        {
+
+
+            Console.WriteLine("Hello, Object Pool!!!");
+
+            HConsole.StarRow();
+            Console.WriteLine("*\tEnter a keyboard shortcut to perform an action");
+            Console.WriteLine("*\tAdd Object: a\n*\tRelease Object: r\n*\tCheck Last Method Calls in pool: p\n*\tCheck Last Method Calls in Console: c\n*\tQuit: q");
+            HConsole.StarRow();
+            bool keepGoBro = true;
+            while (keepGoBro)
+            {
+                string userResponse = Console.ReadLine() ?? "";
+
+                switch (userResponse.ToLower())
+                {
+                    case "a":
+                        AddObject();
+                        break;
+                    case "r":
+                        RemoveObject();
+                        break;
+                    case "p":
+                        objPool.showLastMethodOnAllStoredObjects();
+                        break;
+                    case "c":
+                        ShowLastMethodsCalled();
+                        break;
+                    case "q":
+                        keepGoBro = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("Good Bye Object Pool!!!!");
+            HConsole.StarRow();
+            Console.WriteLine();
+
+            void AddObject()
+            {
+                SharedObject? obj = objPool.Get();
+                if (obj != null)
+                {
+                    if (callMethodA)
+                    {
+                        obj.MethodA();
+                    }
+                    else
+                    {
+                        obj.MethodB();
+                    }
+                    callMethodA = !callMethodA;
+                    sOStack.Push(obj);
+                }
+                else
+                {
+                    Console.WriteLine("Could not create new object, limit is hit");
+                }
+                Console.WriteLine($"Total objects in console: {sOStack.Count}");
+            }
+
+            void RemoveObject()
+            {
+                if (sOStack.Count > 0)
+                {
+                    var toRemove = sOStack.Pop();
+                    toRemove.LastMethodCalled();
+                    objPool.Release(toRemove);
+                }
+                else
+                {
+                    Console.WriteLine("No Objects to release!");
+                }
+                Console.WriteLine($"Total objects in console: {sOStack.Count}");
+            }
+
+            void ShowLastMethodsCalled()
+            {
+                HConsole.StarRow();
+                foreach (var obj in sOStack)
+                {
+                    obj.LastMethodCalled();
+                }
+                HConsole.StarRow();
+            }
+        }
+
+    }
+}
