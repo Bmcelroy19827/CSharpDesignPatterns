9ed893d9ee6b9d173269fa0f07f19ee22beb82f5
Bmcelroy19827 <angelasman2004@yahoo.com>
2022-01-09 09:03:40 -0500

    Created PatternPicker to be the program started with the intention of allowing the user to select an example pattern to see it run.

---
+++ DesignPatterns/PatternPIcker/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelpingHand;
+HConsole.StarRow();
+Console.WriteLine("Hello Patter Picker!!!!");
+Console.WriteLine("Pick a pattern (q to quit):");
+Console.WriteLine("*\tEvent Aggregator: aggregator");
+Console.WriteLine("*\tObject Pool: object pool");
+HConsole.StarRow();
+
+bool keepGoBro = true;
+while (keepGoBro)
+{
+    string userResponse = Console.ReadLine()?? "";
+
+    switch (userResponse.ToLower())
+    {
+        case "aggregator":
+            //new up aggregator
+            break;
+        case "object pool":
+            //new up object pool
+            break;
+        case "q":
+            keepGoBro = false;
+            break;
+        default:
+            break;
+
+    }
+}
