4ed61bf4f9787ef255553d90a0c079abb00f516d
Bmcelroy19827 <angelasman2004@yahoo.com>
2022-01-09 09:02:31 -0500

    Added Helping Hands Class Library to handle universal stuff amongst projects

---
+++ DesignPatterns/HelpingHand/HConsole.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace HelpingHand
+{
+    public static class HConsole
+    {
+        public static void StarRow(int stars = 50)
+        {
+            StringBuilder starBuilder = new StringBuilder();
+            for(int i = 0; i < stars; i++)
+            {
+                starBuilder.Append("*");
+            }
+            Console.WriteLine(starBuilder.ToString());
+        }
+    }
+}
